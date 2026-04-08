cbc2b1b5d0af02f2e61e868a311e080b3e2d228f
Mohammed Zaid <zaidayan456@gmail.com>
2026-04-09 00:47:06 +0530

    clubs page!

---
+++ clubs.js
@@ -0,0 +1,78 @@
+import './style.css';
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/Addons.js';
+import { RoundedBoxGeometry } from 'three/examples/jsm/geometries/RoundedBoxGeometry.js';
+
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+let popcorn;
+let scroll_target  = 0;
+let current_scroll = 0;
+
+camera.position.z = 12
+
+
+const mouse = new THREE.Vector2();
+
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#bg')
+});
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setAnimationLoop(animate);
+
+
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 4); 
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 5);
+directionalLight.position.set(5, 5, 5);
+scene.add(directionalLight);
+
+renderer.render(scene, camera);
+const background_texture = new THREE.TextureLoader().load('/grid_background.png');
+scene.background = background_texture;
+
+
+const geo = new RoundedBoxGeometry(1, 1, 1, 16);
+const tex = new THREE.TextureLoader().load('/clubs.png')
+const mat = new THREE.MeshBasicMaterial({ map: tex })
+const clubs = new THREE.Mesh(geo, mat)
+scene.add(clubs)
+clubs.position.set(4, 0, 6);
+clubs.scale.set(2.8, 2.8, 2.8);
+
+
+function animate() {
+    renderer.render(scene, camera);
+    if (clubs) {
+        current_scroll += (scroll_target - current_scroll) * 0.1;
+        clubs.position.x = 4 + (current_scroll * -0.00001);
+        clubs.position.z = 6 + (current_scroll * -0.005);
+        
+        clubs.rotation.y += 0.01;
+        clubs.rotation.z = current_scroll * -0.001;
+    }
+
+}
+
+function moveClubs() {
+  scroll_target = document.body.getBoundingClientRect().top;
+}
+
+
+window.addEventListener('scroll', moveClubs);
+
+
+
+
+window.addEventListener('load', () => {
+    setTimeout(() => {
+        window.scrollTo(0,0);
+        document.documentElement.scrollTop = 0;
+        document.body.scrollTop = 0;
+        movePopcorn();
+    }, 50);
+});
